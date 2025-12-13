996a1967d28f18693af036fab5e23c9847491e1f
Gerhard <168741987+GerhardJ22@users.noreply.github.com>
2025-12-13 21:33:50 +0700

    Adding vision and mission section to about-us page

---
+++ app/about-us/(components)/layout/VisionMission.tsx
@@ -0,0 +1,28 @@
+import { missionData } from "@/datas/mision_datas";
+import MissionCard from "../ui/MissionCard";
+import VisionCard from "../ui/VisionCard";
+
+
+export default function VisionMission() {
+    return(
+        <section className="w-full bg-slate-100 py-24 ">
+            <div className="flex flex-col container px-6 mx-auto items-center space-y-[100px]">
+
+                {/* Vision */}
+                <VisionCard />
+
+                {/* Mission */}
+                <div className="w-full flex flex-col items-center space-y-10">
+                    <h3 className="text-4xl font-bold text-[#9C27B0]">Our Mission</h3>
+
+                    {/* Grid */}
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-6xl">
+                        {missionData.map((mission) => (
+                            <MissionCard key={mission.id} mission={mission} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ types/mission.d.ts
@@ -0,0 +1,5 @@
+export interface Mission {
+    id: number;
+    title: string;
+    description: string;
+}
