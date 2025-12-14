ebe28005646b4692b24855c6c2d4a56774ff6996
Gerhard <168741987+GerhardJ22@users.noreply.github.com>
2025-12-14 15:08:47 +0700

    update layout and styling for Core Values and Mission components

--- app/about-us/(components)/layout/CoreValues.tsx
+++ app/about-us/(components)/layout/CoreValues.tsx
@@ -3,8 +3,8 @@ import CoreValuesCards from "../ui/CoreValuesCard";
 
 export default function CoreValues() {
     return(
-        <section className="w-full py-24">
-            <div className="flex flex-col container px-5 mx-auto items-center">
+        <section className="w-full py-12">
+            <div className="flex flex-col container px-5 pt-5 pb-12 mx-auto items-center">
 
                 {/* Section Title */}
                 <h2 className="text-4xl font-bold text-[#1E293B] mb-15">
@@ -12,7 +12,7 @@ export default function CoreValues() {
                 </h2>
 
                 {/* Grid */}
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-11">
                     {coreValuesData.map((coreValue) => (
                         <CoreValuesCards key={coreValue.id} coreValue={coreValue} />
                     ))}
--- app/about-us/(components)/ui/CoreValuesCard.tsx
+++ app/about-us/(components)/ui/CoreValuesCard.tsx
@@ -9,16 +9,26 @@ interface CoreValuesCardsProps {
 export default function CoreValuesCards({ coreValue }: CoreValuesCardsProps) {
     const Icon = coreValue.icon;
     return (
-        <>
-            <Card className="w-full p-8 rounded-2xl shadow-lg">
-                <CardHeader className="flex text-[#9C27B0]">
-                        <Icon />
+            <Card className="group h-full flex flex-col p-8 rounded-2xl bg-[#F8FAFC] shadow-sm transition-all hover:shadow-md hover:-translate-y-1">
+                <CardHeader className="p-0">
+
+                    {/* Icon */}
+                    <div className="flex items-center justify-center text-[#9C27B0] w-14 h-14 bg-white rounded-2xl ">
+                        <Icon className="w-8 h-8 group-hover:text-primary/80"/>
+                    </div>
+
                 </CardHeader>
-                <CardContent className="space-y-4">
-                    <CardTitle>{coreValue.title}</CardTitle>
-                    <CardDescription>{coreValue.description}</CardDescription>
+                <CardContent className="p-0 -mt-2.5 space-y-4 flex-1">
+
+                    {/* Title & Description */}
+                    <CardTitle className="text-[#1E293B] font-bold text-xl">
+                        {coreValue.title}
+                    </CardTitle>
+                    <CardDescription className="text-base text-[#64748B] leading-relaxed">
+                        {coreValue.description}
+                    </CardDescription>
+
                 </CardContent>
             </Card>
-        </>
     );
 }
