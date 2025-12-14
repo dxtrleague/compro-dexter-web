2c35625892c16b439c94a5e8b164666a57cb15a4
dentowahyuu <wahyudento08@gmail.com>
2025-12-14 08:44:48 +0700

    feat: add data clients and partners

--- datas/client_data.ts
+++ datas/client_data.ts
@@ -1,50 +1,90 @@
 import { Client } from '@/types/client';
+import  ac  from "@/public/assets/client/ackita.jpg";
+import  alfa  from "@/public/assets/client/alfa.jpg";
+import  bakau  from "@/public/assets/client/bakaumu.png";
+import  bz  from "@/public/assets/client/bzwedding.png";
+import  sayur  from "@/public/assets/client/carisayur.png";
+import  elevenia  from "@/public/assets/client/elevenia.png";
+import  montree  from "@/public/assets/client/montree.png";
+import  ocbc  from "@/public/assets/client/ocbc.png";
+import  paskomnas  from "@/public/assets/client/paskomnas.png";
+import  poskota  from "@/public/assets/client/poskota.png";
+import  rasamala  from "@/public/assets/client/rasamala.jpg";
+import  smf  from "@/public/assets/client/smf.jpg";
+import  telkom  from "@/public/assets/client/telkom.png";
+import  ultravoucher  from "@/public/assets/client/ultravoucher.png";
+
 
 
 export const clientData: Client[] = [
   {
     id: 1,
     name: "Client One",
-    logo: "/assets/client/logo-1.png",
+    logo: ac.src,
   },
   {
     id: 2,
     name: "Client Two",
-    logo: "/assets/client/logo-2.png",
+    logo: alfa.src,
   },
   {
     id: 3,
     name: "Client Three",
-    logo: "/assets/client/logo-3.png",
+    logo: bakau.src,
   },
   {
     id: 4,
     name: "Client Four",
-    logo: "/assets/client/logo-4.png",
+    logo: bz.src,
   },
   {
     id: 5,
     name: "Client Five",
-    logo: "/assets/client/logo-5.png",
+    logo: sayur.src,
   },
   {
     id: 6,
     name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    logo: elevenia.src,
   },
   {
     id: 7,
-    name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    name: "Client Seven",
+    logo: montree.src,
   },
   {
     id: 8,
-    name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    name: "Client Eight",
+    logo: ocbc.src,
   },
   {
     id: 9,
-    name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    name: "Client Nine",
+    logo: paskomnas.src,
+  },
+  {
+    id: 10,
+    name: "Client Ten",
+    logo: poskota.src,
+  },
+  {
+    id: 11,
+    name: "Client Eleven",
+    logo: rasamala.src,
+  },
+  {
+    id: 12,
+    name: "Client Twelve",
+    logo: smf.src,
+  },
+  {
+    id: 13,
+    name: "Client Thirteen",
+    logo: telkom.src,
+  },
+  {
+    id: 14,
+    name: "Client Fourteen",
+    logo: ultravoucher.src,
   }
 ];
--- datas/partner_data.ts
+++ datas/partner_data.ts
@@ -1,50 +1,77 @@
 import { Partner } from '@/types/partner';
+import  antiKode  from "@/public/assets/partners/antikode.jpg";
+import  bayarinID  from "@/public/assets/partners/bayarinid.jpg";
+import  evorts  from "@/public/assets/partners/evorts.png";
+import  justAbout  from "@/public/assets/partners/justabout.jpg";
+import  kelindoku  from "@/public/assets/partners/kelindoku.jpg";
+import  meta  from "@/public/assets/partners/metatech.jpg";
+import  pulsa  from "@/public/assets/partners/mobilepulsa.png";
+import  sagara  from "@/public/assets/partners/sagara.png";
+import  sixtyTwo  from "@/public/assets/partners/sixtytwo.png";
+import  sonia  from "@/public/assets/partners/sonia.png";
+import  sprint  from "@/public/assets/partners/sprint.png";
+import  tic  from "@/public/assets/partners/tic.jpg";
 
 
 export const partnerData: Partner[] = [
   {
     id: 1,
     name: "Client One",
-    logo: "/assets/client/logo-1.png",
+    logo: antiKode.src,
   },
   {
     id: 2,
     name: "Client Two",
-    logo: "/assets/client/logo-2.png",
+    logo: bayarinID.src,
   },
   {
     id: 3,
     name: "Client Three",
-    logo: "/assets/client/logo-3.png",
+    logo: evorts.src,
   },
   {
     id: 4,
     name: "Client Four",
-    logo: "/assets/client/logo-4.png",
+    logo: justAbout.src,
   },
   {
     id: 5,
     name: "Client Five",
-    logo: "/assets/client/logo-5.png",
+    logo: kelindoku.src,
   },
   {
     id: 6,
     name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    logo: meta.src,
   },
   {
     id: 7,
-    name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    name: "Client Seven",
+    logo: pulsa.src,
   },
   {
     id: 8,
-    name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    name: "Client Eight",
+    logo: sagara.src,
   },
   {
     id: 9,
-    name: "Client Six",
-    logo: "/assets/client/logo-6.png",
+    name: "Client Nine",
+    logo: sixtyTwo.src,
+  },
+  {
+    id: 10,
+    name: "Client Ten",
+    logo: sonia.src,
+  },
+  {
+    id: 11,
+    name: "Client Eleven",
+    logo: sprint.src,
+  },
+  {
+    id: 12,
+    name: "Client Twelve",
+    logo: tic.src,
   }
 ];
