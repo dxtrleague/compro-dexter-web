a79bb191d7f1b6ca40fd63eadd6ac138b600637c
dentowahyuu <wahyudento08@gmail.com>
2025-12-17 21:23:21 +0700

    build(config): remove basePath and assetPrefix from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/compro-dexter-web',
-  assetPrefix: isProd ? '/compro-dexter-web/' : '', 
+  
   images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true, 
+    unoptimized: true, 
   },
 };
 
