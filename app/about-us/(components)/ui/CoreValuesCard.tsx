b4d173ad36c22fcaf17f967528f1f48251244b38
Gerhard <168741987+GerhardJ22@users.noreply.github.com>
2025-12-23 01:42:13 +0700

    refactor(ui): enhance layout and styling across CoreValues, HeroSection, VisionMission, and card components

--- app/about-us/(components)/ui/CoreValuesCard.tsx
+++ app/about-us/(components)/ui/CoreValuesCard.tsx
@@ -13,18 +13,18 @@ export default function CoreValuesCards({ coreValue }: CoreValuesCardsProps) {
                 <CardHeader className="p-0">
 
                     {/* Icon */}
-                    <div className="flex items-center justify-center text-[#9C27B0] w-14 h-14 bg-white rounded-2xl ">
-                        <Icon className="w-8 h-8 group-hover:text-primary/80"/>
+                    <div className="flex items-center justify-center text-muted w-13 h-13 md:w-14 md:h-14 bg-white rounded-2xl ">
+                        <Icon className="w-7 h-7 md:w-8 md:h-8 group-hover:text-primary/80"/>
                     </div>
 
                 </CardHeader>
-                <CardContent className="p-0 -mt-2.5 space-y-4 flex-1">
+                <CardContent className="p-0 -mt-2.5 space-y-2 md:space-y-4 flex-1">
 
                     {/* Title & Description */}
-                    <CardTitle className="text-[#1E293B] font-bold text-xl">
+                    <CardTitle className="text-primary-foreground font-bold text-lg md:text-[22px]">
                         {coreValue.title}
                     </CardTitle>
-                    <CardDescription className="text-base text-[#64748B] leading-relaxed">
+                    <CardDescription className="text-[15px] md:text-lg text-secondary-foreground leading-relaxed">
                         {coreValue.description}
                     </CardDescription>
 
